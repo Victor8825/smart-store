bd5954a2633d8e52a580b16d7791461f8622a441
Victor8825 <95580152+Victor8825@users.noreply.github.com>
2022-12-09 17:43:36 +0000

    Deploying to gh-pages from @ Victor8825/smart-store@b6d2e43d200453bfecb780d300d77780126c021b 🚀

---
+++ login.0334bc85.js
@@ -0,0 +1,2 @@
+loginFormRef=document.querySelector(".login_form"),console.log(loginFormRef);
+//# sourceMappingURL=login.0334bc85.js.map
---
+++ login.71654efe.js
@@ -0,0 +1,2 @@
+loginFormRef=document.querySelector(".login_form"),console.log(loginFormRef);
+//# sourceMappingURL=login.71654efe.js.map
