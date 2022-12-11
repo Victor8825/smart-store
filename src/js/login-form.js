09227926a61a0d4a5f2eae3b9fe10d8ce88068c5
dkrasovskyi <dkrasovskyi@sardinasystems.com>
2022-12-11 22:23:26 +0200

    login-form: add checks to js

--- src/js/login-form.js
+++ src/js/login-form.js
@@ -1,11 +1,32 @@
-const loginFormInputRef = document.querySelector('.login-form__password');
+const loginFormPhoneInputRef = document.querySelector('.login-form__phone');
+const loginFormPasswordInputRef = document.querySelector('.login-form__password');
+const loginFormSubmitRef = document.querySelector('.login-form__btn')
+const loginFormErrorRef = document.querySelector('.login-form__password-error');
 const loginFormInputIcon = document.querySelector('.eye-open');
 
 loginFormInputIcon.addEventListener('mousedown', () => {
-  loginFormInputRef.setAttribute('type', 'text');
+  loginFormPasswordInputRef.setAttribute('type', 'text');
 });
 
 loginFormInputIcon.addEventListener('mouseup', () => {
-  loginFormInputRef.setAttribute('type', 'password');
+  loginFormPasswordInputRef.setAttribute('type', 'password');
 });
 
+
+function loginFormCheckInput() {
+  if (loginFormPhoneInputRef.value.length == 15 && loginFormPasswordInputRef.value.length >= 6) {
+    // enable submit button
+    loginFormSubmitRef.disabled = false;
+  } else {
+    //disable submit button
+    loginFormSubmitRef.disabled = true;
+  }
+  if (loginFormPasswordInputRef.value.length < 6) {
+    loginFormErrorRef.classList.add('login-form__password-error--visible')
+  } else {
+    loginFormErrorRef.classList.remove('login-form__password-error--visible')
+  }
+}
+
+loginFormPhoneInputRef.addEventListener('input', loginFormCheckInput);
+loginFormPasswordInputRef.addEventListener('input', loginFormCheckInput)
